da58695580a13d110d8a02911c12f6c737978a07
John Blank <veloblank@gmail.com>
2019-11-15 15:07:58 -0500

    Build out event listener for hidden divs

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
 window.load = addEventListeners()
 
 function addEventListeners() {
-  let radioButtons = document.getElementsByClassName('toggle-radio')
-  console.log(radioButtons)
+  let radioButtons = document.querySelectorAll('input[type=radio][name="nav-toggle"]')
+  for (let button of radioButtons) {
+    button.addEventListener('click', function () {
+      this.parentNode.parentNode.attributes['data-view'] = this.value
+      let views = document.querySelectorAll('.views')
+      let view = document.getElementById(this.value)
+      views.forEach(div => {
+        div.style.display = "none"
+      });
+      view.style.display = "block"
+    })
+  }
 }
