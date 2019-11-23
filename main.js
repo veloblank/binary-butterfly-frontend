5d962f402f9395772fbe5e6286840f4b874cb95a
John Blank <veloblank@gmail.com>
2019-11-22 20:51:15 -0500

    Adds Google sigin modal

---
+++ main.js
@@ -0,0 +1,10 @@
+let modal = document.getElementById('signin-modal');
+let openModalBtn = document.getElementById('sign-in-button');
+let closeModalBtn = document.getElementById('modal-close-btn');
+
+
+openModalBtn.addEventListener('click', openModal);
+
+function openModal() {
+  console.log(123)
+}
--- src/App.js
+++ src/App.js
@@ -1,6 +1,31 @@
 window.load = addEventListeners()
 
 function addEventListeners() {
+  let modal = document.getElementById('signin-modal');
+  let openModalBtn = document.getElementById('signin-button');
+  let closeModalBtn = document.getElementById('modal-close-btn');
+
+
+  openModalBtn.addEventListener('click', openModal);
+  closeModalBtn.addEventListener('click', closeModal);
+  //Outside 'click' event of modal
+
+  window.addEventListener('click', clickOutside);
+
+  function openModal() {
+    modal.style.display = "block";
+  }
+
+  function closeModal() {
+    modal.style.display = "none";
+  }
+
+  function clickOutside(e) {
+    if (e.target === modal) {
+      modal.style.display = "none";
+    }
+  }
+
   let radioButtons = document.querySelectorAll('input[type=radio][name="nav-toggle"]')
   for (let button of radioButtons) {
     button.addEventListener('click', function () {
