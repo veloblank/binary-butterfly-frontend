bdfaffc0341356f99a675e9b0bf5947f1956f1c5
John Blank <veloblank@gmail.com>
2019-11-28 05:19:43 -0500

    Start moving App to own class

--- src/App.js
+++ src/App.js
@@ -1,74 +1,59 @@
-import fetchSportsProps from './sportsPropsData.js';
+import Modal from './Modal.js'
 import SportsProp from './SportsProp.js';
 import Slider from './Slider.js';
+import fetchSportsProps from './sportsPropsData.js';
 
-window.onload = buildSportsProps;
 
-function buildSportsProps() {
-  fetchSportsProps()
-    .then(arr => {
-      for (let obj of arr) {
-        let prop = new SportsProp(obj);
-        prop.render();
-        let slider = new Slider(prop);
-        slider.render();
-      }
-    });
-};
-
-setTimeout(() => {
-  addEventListeners();
-}, 300);
-
-function addEventListeners() {
-  let sliders = document.getElementsByClassName('slider');
-  for (let slider of sliders) {
-    slider.addEventListener('input', e => {
-      if (e.target.value > 0) {
-        document.getElementById(`prop-${e.target.dataset.id}-home-values`).innerHTML = e.target.value * 10 / 10;
-        document.getElementById(`prop-${e.target.dataset.id}-away-values`).innerHTML = Math.round((e.target.value * 30) / 10) * (-1);
-      } else {
-        document.getElementById(`prop-${e.target.dataset.id}-away-values`).innerHTML = e.target.value * (-10) / 10;
-        document.getElementById(`prop-${e.target.dataset.id}-home-values`).innerHTML = Math.round((e.target.value * 30) / 10);
-      }
-    });
+class App {
+  constructor() {
+    this.modal = new Modal();
+    this.state = {};
+    console.log("Application Starting...");
+    this.buildProps();
   }
 
-  let modal = document.getElementById('signin-modal');
-  let openModalBtn = document.getElementById('signin-button');
-  let closeModalBtn = document.getElementById('modal-close-btn');
-
-
-  openModalBtn.addEventListener('click', openModal);
-  closeModalBtn.addEventListener('click', closeModal);
-  //Outside 'click' event of modal
-
-  window.addEventListener('click', clickOutside);
-
-  function openModal() {
-    modal.style.display = "block";
+  buildProps() {
+    fetchSportsProps()
+      .then(arr => {
+        for (let obj of arr) {
+          obj = new SportsProp(obj);
+          obj.render();
+          let slider = new Slider(obj);
+          slider.render();
+        }
+      })
+    this.addEventListeners();
   }
 
-  function closeModal() {
-    modal.style.display = "none";
-  }
+  addEventListeners() {
+    this.modal.addModalListeners();
 
-  function clickOutside(e) {
-    if (e.target === modal) {
-      modal.style.display = "none";
+    let sliders = document.getElementsByClassName('slider');
+    for (let slider of sliders) {
+      slider.addEventListener('input', e => {
+        if (e.target.value > 0) {
+          document.getElementById(`prop-${e.target.dataset.id}-home-values`).innerHTML = e.target.value * 10 / 10;
+          document.getElementById(`prop-${e.target.dataset.id}-away-values`).innerHTML = Math.round((e.target.value * 30) / 10) * (-1);
+        } else {
+          document.getElementById(`prop-${e.target.dataset.id}-away-values`).innerHTML = e.target.value * (-10) / 10;
+          document.getElementById(`prop-${e.target.dataset.id}-home-values`).innerHTML = Math.round((e.target.value * 30) / 10);
+        }
+      });
+    }
+
+
+    let radioButtons = document.querySelectorAll('input[type=radio][name="nav-toggle"]');
+    for (let button of radioButtons) {
+      button.addEventListener('click', function () {
+        this.parentNode.parentNode.attributes['data-view'] = this.value;
+        let views = document.querySelectorAll('.views');
+        let view = document.getElementById(this.value);
+        views.forEach(div => {
+          div.style.display = "none";
+        });
+        view.style.display = "block";
+      });
     }
   }
-
-  let radioButtons = document.querySelectorAll('input[type=radio][name="nav-toggle"]');
-  for (let button of radioButtons) {
-    button.addEventListener('click', function () {
-      this.parentNode.parentNode.attributes['data-view'] = this.value;
-      let views = document.querySelectorAll('.views');
-      let view = document.getElementById(this.value);
-      views.forEach(div => {
-        div.style.display = "none";
-      });
-      view.style.display = "block";
-    });
-  }
 }
+export default App;
