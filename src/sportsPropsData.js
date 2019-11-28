a273125da670ff418487e68ffcadc3771c385cf8
John Blank <veloblank@gmail.com>
2019-11-28 05:03:46 -0500

    Create index.js file and separate App file

--- src/sportsPropsData.js
+++ src/sportsPropsData.js
@@ -1,4 +1,4 @@
-const fetchSportsProps = () => {
+function fetchSportsProps() {
   return fetch('http://localhost:3000/api/v1/contest_weeks/2/contest_props')
     .then(response => response.json());
 }
