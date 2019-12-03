76a61e068874c56755e0cd4fef2891280a8aa7af
John Blank <veloblank@gmail.com>
2019-12-03 15:21:46 -0500

    Builds logged_in method

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
-import * as fetchFunc from './fetchRequests.js';
-import Slider from './Slider.js';
-import SportsProp from './SportsProp.js';
-import * as mathFuncs from './mathFuncs.js';
-import * as DomEventListeners from './listeners/DomEventListeners.js';
+import * as fetchFunc from "./fetchRequests.js";
+import Slider from "./Slider.js";
+import SportsProp from "./SportsProp.js";
+import * as mathFuncs from "./mathFuncs.js";
+import * as DomEventListeners from "./listeners/DomEventListeners.js";
 
 class App {
   constructor() {
     this.state = {
+      userLoggedIn: this.loggedIn(),
       user_id: "",
-      user: "",
+      user: null,
       props: [],
       date: new Date()
     };
     this.buildProps();
   }
 
+  loggedIn() {
+    this.state.user ? true : false;
+    console.log(this.state);
+  }
+
   buildProps() {
-    fetchFunc.fetchSportsProps()
+    fetchFunc
+      .fetchSportsProps()
       .then(props => {
         for (let obj of props) {
           let slider = new Slider(obj);
@@ -25,7 +32,9 @@ class App {
           slider.render();
         }
       })
-      .catch(error => console.log("There was an error connecting to the server: \n", error));
+      .catch(error =>
+        console.log("There was an error connecting to the server: \n", error)
+      );
 
     //sets delay so Props can be fetched and built before Listeners are added
     setInterval(() => {
@@ -34,10 +43,12 @@ class App {
   }
 
   addEventListeners() {
-    let radioButtons = document.querySelectorAll('input[type=radio][name="nav-toggle"]');
+    let radioButtons = document.querySelectorAll(
+      'input[type=radio][name="nav-toggle"]'
+    );
     for (let button of radioButtons) {
-      button.addEventListener('click', () => {
-        let views = document.querySelectorAll('.views');
+      button.addEventListener("click", () => {
+        let views = document.querySelectorAll(".views");
         let view = document.getElementById(button.value);
         views.forEach(div => {
           div.style.display = "none";
@@ -46,53 +57,71 @@ class App {
       });
     }
 
-    document.getElementById("create-account-form").addEventListener("submit", (e) => {
-      e.preventDefault();
-      fetchFunc.fetchCreateUser(this);
-    });
+    document
+      .getElementById("create-account-form")
+      .addEventListener("submit", e => {
+        e.preventDefault();
+        fetchFunc.fetchCreateUser(this);
+      });
 
-    document.getElementById('open-create-account-modal').addEventListener('click', (e) => {
-      let modal = document.getElementById('create-account-modal');
-      e.preventDefault();
-      modal.style.display = "block";
-    });
+    document
+      .getElementById("open-create-account-modal")
+      .addEventListener("click", e => {
+        let modal = document.getElementById("create-account-modal");
+        e.preventDefault();
+        modal.style.display = "block";
+      });
 
-    document.getElementById('create-modal-close-btn').addEventListener('click', () => {
-      let modal = document.getElementById('create-account-modal');
-      modal.style.display = "none";
-    });
+    document
+      .getElementById("create-modal-close-btn")
+      .addEventListener("click", () => {
+        let modal = document.getElementById("create-account-modal");
+        modal.style.display = "none";
+      });
 
-    document.getElementById('open-signin-account-modal').addEventListener('click', (e) => {
-      let modal = document.getElementById('signin-modal');
-      modal.style.display = "block";
-    });
+    document
+      .getElementById("open-signin-account-modal")
+      .addEventListener("click", e => {
+        let modal = document.getElementById("signin-modal");
+        modal.style.display = "block";
+      });
 
-    document.getElementById('signin-modal-close-btn').addEventListener('click', () => {
-      let modal = document.getElementById('signin-modal');
-      modal.style.display = "none";
-    });
+    document
+      .getElementById("signin-modal-close-btn")
+      .addEventListener("click", () => {
+        let modal = document.getElementById("signin-modal");
+        modal.style.display = "none";
+      });
 
-    window.addEventListener('click', (e) => {
-      let createModal = document.getElementById('create-account-modal');
-      let signinModal = document.getElementById('signin-modal');
+    window.addEventListener("click", e => {
+      let createModal = document.getElementById("create-account-modal");
+      let signinModal = document.getElementById("signin-modal");
       if (createModal === e.target || signinModal === e.target) {
         e.target.style.display = "none";
       }
     });
 
-    let sliders = document.getElementsByClassName('slider');
+    let sliders = document.getElementsByClassName("slider");
     for (let slider of sliders) {
-      slider.addEventListener('input', e => {
+      slider.addEventListener("input", e => {
         if (e.target.value > 0) {
-          document.getElementById(`prop-${e.target.dataset.id}-home-values`).innerHTML = e.target.value * 10 / 10;
-          document.getElementById(`prop-${e.target.dataset.id}-away-values`).innerHTML = Math.round((e.target.value * 30) / 10) * (-1);
+          document.getElementById(
+            `prop-${e.target.dataset.id}-home-values`
+          ).innerHTML = (e.target.value * 10) / 10;
+          document.getElementById(
+            `prop-${e.target.dataset.id}-away-values`
+          ).innerHTML = Math.round((e.target.value * 30) / 10) * -1;
         } else {
-          document.getElementById(`prop-${e.target.dataset.id}-away-values`).innerHTML = e.target.value * (-10) / 10;
-          document.getElementById(`prop-${e.target.dataset.id}-home-values`).innerHTML = Math.round((e.target.value * 30) / 10);
+          document.getElementById(
+            `prop-${e.target.dataset.id}-away-values`
+          ).innerHTML = (e.target.value * -10) / 10;
+          document.getElementById(
+            `prop-${e.target.dataset.id}-home-values`
+          ).innerHTML = Math.round((e.target.value * 30) / 10);
         }
       });
 
-      slider.addEventListener('mouseup', () => {
+      slider.addEventListener("mouseup", () => {
         mathFuncs.displayPoints();
         //TODO: Make patch req to server that shows the user has made this pick
       });
