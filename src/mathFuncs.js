8ec7c045a41a87489b7191b22227373a82359b61
John Blank <veloblank@gmail.com>
2019-12-03 00:14:53 -0500

    Create new file to handle math funcs of app

---
+++ src/mathFuncs.js
@@ -0,0 +1,14 @@
+function displayPoints() {
+  let sliders = [...document.getElementsByClassName('slider')];
+  let numOfPicksMade;
+  let numOfProps = sliders.length;
+  let filtered = sliders.filter(slider => {
+    return (parseFloat(slider.value) != 0);
+  });
+  numOfPicksMade = filtered.length;
+  document.querySelector('#daily-total-picks-made').innerHTML = numOfPicksMade;
+  document.querySelector('#helper').style.display = "inline";
+  document.querySelector('#daily-total-picks').innerHTML = numOfProps;
+}
+
+export default displayPoints;
