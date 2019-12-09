c0f65b03c6a0d96b231697b2b8134ad089543d19
John Blank <veloblank@gmail.com>
2019-12-09 09:11:44 -0500

    Hide buttons when login class added

--- src/listeners/modalListeners.js
+++ src/listeners/modalListeners.js
@@ -40,6 +40,10 @@ function addModalListeners() {
 function closeModal() {
   let modal = document.getElementById("signin-account-modal");
   modal.style.display = "none";
+  let signOutBtn = document.getElementById('signout-button')
+  signOutBtn.classList.add('signed-in')
+  let signInBtn = document.getElementById('open-signin-account-modal')
+  signInBtn.classList.add('signed-in')
 }
 
 export { addModalListeners, closeModal };
