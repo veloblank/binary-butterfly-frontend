e4f8fe9de2f84f92381dee27dc116e62465e2f74
John Blank <veloblank@gmail.com>
2019-12-14 18:41:48 -0500

    Move listeners to App

--- src/App.js
+++ src/App.js
@@ -1,37 +1,61 @@
 import SportsPropBuilder from './SportsPropBuilder.js';
-import {
-  addModalListeners
-} from './listeners/modalListeners.js'
+import * as fetchRequests from './fetchRequests.js'
 
 class App {
   constructor() {
     this.state = {
       sportsProps: [],
-      userLoggedIn: null,
-      user_id: "",
+      user_id: '',
       user: null,
       date: new Date(),
     };
-    this.propBuilder = new SportsPropBuilder(this.state.sportsProps);
+
+    this.propBuilder = new SportsPropBuilder();
     this.state.sportsProps = this.propBuilder.adapter.sportsProps;
     this.addEventListeners();
   }
 
-  loggedIn(data) {
-    if (data.username) {
-      this.state.user_id = data.id;
-      this.state.user = data.username;
-      this.userLoggedIn = true;
-      //TODO:
-      document.getElementsByClassName('user-display').classList.remove('logged-out');
-      document.getElementsByClassName('user-display').classList.add('logged-in');
-      document.getElementById('account-user').innerText = this.state.user;
-    }
-    return this.state.user ? true : false;
-  }
-
+  // loggedIn() {
+  //   console.log("Checking login")
+  //   document.getElementsByClassName('user-display').classList.remove('logged-out');
+  //   document.getElementsByClassName('user-display').classList.add('logged-in');
+  //   document.getElementById('account-user').innerText = this.state.user;
+  // }
   addEventListeners() {
-    addModalListeners();
+    document
+      .getElementById("open-signin-account-modal")
+      .addEventListener("click", e => {
+        let modal = document.getElementById("signin-account-modal");
+        modal.style.display = "block";
+      });
+
+    //Click 'X' inside modal  
+    document
+      .getElementById("signin-modal-close-btn")
+      .addEventListener("click", () => {
+        let modal = document.getElementById("signin-account-modal");
+        modal.style.display = "none";
+      });
+
+    //Click submit in Modal
+    document
+      .getElementById('signin-account-form')
+      .addEventListener("submit", e => {
+        e.preventDefault();
+        fetchRequests.fetchCreateUser(e);
+        //alert("You are signed in!")
+      });
+
+
+    //Click outside of Modal  
+    window.addEventListener("click", e => {
+      let signinModal = document.getElementById("signin-account-modal");
+      if (signinModal === e.target) {
+        e.target.style.display = "none";
+      }
+    });
+
   }
 }
+
 export default App;
